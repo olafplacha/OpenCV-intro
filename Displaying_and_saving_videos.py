7e3119f7cfbed26383aabb2c92f5628b521e6b68
Olaf Placha <olafplacha@gmail.com>
2017-10-26 20:41:39 +0200

    Add files via upload

---
+++ Displaying_and_saving_videos.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[45]:
+
+#importing useful libraries
+import cv2
+import numpy as np
+
+
+# In[46]:
+
+#capturing video from video camera
+cap = cv2.VideoCapture(0)
+#saving the video
+out = cv2.VideoWriter('output.avi', -1, 10.0, (640,480))
+
+
+# In[47]:
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
+    
+    #saving frames to out var
+    out.write(frame)
+    
+    #break if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#releasing the camera
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
